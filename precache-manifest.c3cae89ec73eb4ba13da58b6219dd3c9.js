4e24caccd61e47e8a14c74583f3fdcc5e09b6197
Sneha Kalita <sneha3950@gmail.com>
2019-06-11 15:39:01 +0530

    Updates

---
+++ precache-manifest.c3cae89ec73eb4ba13da58b6219dd3c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c689218ce411a5a771291652c87e7a7",
+    "url": "/react-memegenerator/index.html"
+  },
+  {
+    "revision": "402aac9f5f2a1b42954a",
+    "url": "/react-memegenerator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-memegenerator/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "402aac9f5f2a1b42954a",
+    "url": "/react-memegenerator/static/js/main.14553c1f.chunk.js"
+  },
+  {
+    "revision": "aa59356cec2923c0c20e",
+    "url": "/react-memegenerator/static/js/runtime~main.d17c967c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-memegenerator/static/media/logo.5d5d9eef.svg"
+  }
+]);
